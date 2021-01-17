9874e28c4c64c76fd541e8a52ff672acf93b51fe
LeonTeraoka <68591897+LeonTeraoka@users.noreply.github.com>
2021-01-17 13:51:09 +0900

    文字列のシンタックスエラーを避ける

---
+++ syntaxerror.py
@@ -0,0 +1,8 @@
+print("文字列のシンタックスエラーを避ける")
+
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+message = 'One of Python's strengths is its diverse community.'
+print(message)
+
