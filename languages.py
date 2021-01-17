3139842d601b38541294a536714d9f5fada3b701
LeonTeraoka <68591897+LeonTeraoka@users.noreply.github.com>
2021-01-17 13:42:22 +0900

    空白文字を取り除く

--- languages.py
+++ languages.py
@@ -3,3 +3,22 @@ print ("Python")
 print("\tPython")
 
 print("Languages:\nPython\nC\nJavascript")
+
+print ("--------------------------------")
+
+favorite_language = "python "
+print (favorite_language + "a")
+print (favorite_language.rstrip() + "a")
+
+print ("---------------------------------")
+
+favorite_language = favorite_language.rstrip()
+print (favorite_language + "a")
+
+print ("--------------------------------")
+
+favorite_language = " Python "
+print (favorite_language.rstrip() + "a")
+print ("a" + favorite_language.lstrip())
+print ("a" + favorite_language.strip() + "a")
+
