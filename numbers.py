157441a2b878a2929d4abbc580fb4e40d3951d70
LeonTeraoka <68591897+LeonTeraoka@users.noreply.github.com>
2021-01-17 16:44:06 +0900

    数値

---
+++ numbers.py
@@ -0,0 +1,34 @@
+print (2 + 3)
+print (2 - 3)
+print (2 * 3)
+print (3 / 2)
+print (3 ** 2)
+print (3 ** 3)
+print (10 ** 6)
+print (2 + 3*4)
+print ((2 + 3) * 4)
+print (0.1 + 0.1)
+print (0.2 + 0.2)
+print ("---------------------")
+print (2 * 0.1)
+print (2 * 0.2)
+print ("---------------------")
+print (0.2 + 0.1)
+print (3 * 0.1)
+print ("---------------------")
+print (4/2)
+print (1 + 2.0)
+print (2 * 3.0)
+print (3.0 ** 2)
+print ("---------------------")
+print ("アンダーバーで数字を読みやすくする")
+universe_age = 14_000_000_000
+print (universe_age)
+print ("---------------------")
+x, y, z = 0, 0, 0
+print (x)
+print ("---------------------")
+MAX_CONNECTIONS = 5000
+
+
+
