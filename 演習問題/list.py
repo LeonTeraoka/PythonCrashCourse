f7333013b88871d8f449dd48b4beb76145a9828a
LeonTeraoka <68591897+LeonTeraoka@users.noreply.github.com>
2021-01-21 23:43:04 +0900

    演習問題3-1-3

---
+++ 演習問題/list.py
@@ -0,0 +1,13 @@
+names = ["me", "you", "he", "her"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[-1])
+print("---------------")
+print(f"Hello{names[0].title()}, nice to meet you")
+print(f"Hello{names[1].title()}, nice to meet you")
+print(f"Hello{names[2].title()}, nice to meet you")
+print(f"Hello{names[3].title()}, nice to meet you")
+print("--------------")
+wishlist = ("iphone", "ipad")
+print(f"私は{wishlist[0].title()}がほしい。")
