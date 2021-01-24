40a46704be37b12d3be4322305af4118c2e71e46
LeonTeraoka <68591897+LeonTeraoka@users.noreply.github.com>
2021-01-24 14:16:20 +0900

    リストから要素を削除

--- motorcycles.py
+++ motorcycles.py
@@ -3,3 +3,43 @@ print(motorcycles)
 
 motorcycles[0] = "ducati"
 print(motorcycles)
+
+motorcycles.append("honda")
+print(motorcycles)
+print("------------------")
+
+motorcycles = []
+motorcycles.append("honda")
+motorcycles.append("yamaha")
+motorcycles.append("suzuki")
+print(motorcycles)
+motorcycles.insert(0, "ducati")
+print(motorcycles)
+print("--------------------")
+
+del motorcycles[0]
+del motorcycles[1]
+print(motorcycles)
+print("-------------------")
+
+motorcycles = ["honda", "yamaha", "suzuki"]
+print(motorcycles)
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+last_owned = motorcycles.pop(0)
+print(f"最近手に入れたバイクは{last_owned.title()}です。")
+print(motorcycles)
+too_expensive = "yamaha"
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\n{too_expensive.title()}は私には高すぎます。")
+print("--------------------")
+motorcycles = ["honda", "yamaha", "suzuki"]
+motorcycles.append("ducati")
+print(motorcycles)
+too_expensive = "ducati"
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\n{too_expensive.title()}は私には高すぎます。")
+
