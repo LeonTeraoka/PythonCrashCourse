156bb3d80915fc7a5eb90689c848fddef69de9f2
LeonTeraoka <68591897+LeonTeraoka@users.noreply.github.com>
2021-01-24 23:51:25 +0900

    sorted()関数でリストを一時的にソート

--- cars.py
+++ cars.py
@@ -6,3 +6,11 @@ print(cars)
 cars = ["bmw", "アウディ", "日産", "すばる"]
 cars.sort()
 print(cars)
+print("-------------------")
+cars = ["bmw", "audi", "toyota", "subaru"]
+print("元のリスト")
+print(cars)
+print("\nソートされたリスト")
+print(sorted(cars))
+print("\n元のリストを再表示")
+print(cars)
