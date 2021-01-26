199e6eb3ad43d307a0832e266b4b65b61b27ee4d
LeonTeraoka <68591897+LeonTeraoka@users.noreply.github.com>
2021-01-26 23:39:03 +0900

    演習問題3-8

---
+++ 演習問題/travel.py
@@ -0,0 +1,15 @@
+world = ["USA", "Japan", "UK", "China", "India"]
+print(world)
+print(sorted(world))
+print(world)
+world.reverse()
+print(world)
+world.reverse()
+print(world)
+#world.sort()
+world.sort(reverse=True)
+print(world)
+
+
+
+
