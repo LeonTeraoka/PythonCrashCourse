15b75ce584d3af2e6af4fe0bcfa28d9488e94d81
LeonTeraoka <68591897+LeonTeraoka@users.noreply.github.com>
2021-01-31 03:26:56 +0900

    forループの後に何かを実行

--- magicians.py
+++ magicians.py
@@ -2,7 +2,9 @@ magicians = ["alice", "david", "carolina"]
 for magician in magicians:
 	print (f"{magician.title()}は素晴らしい手品を演じた！")
 	print (f"{magician.title()}の次の手品が待ちきれない。\n") #\nにより最後に空行
-	
+
+print("みなさんありがとう。素晴らしい手品ショーでした！")
+
 
 
 	
