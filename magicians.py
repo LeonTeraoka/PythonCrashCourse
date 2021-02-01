96e8a48a808531064fe694d88c3d626a0625cc29
LeonTeraoka <brt765g@gmail.com>
2021-02-02 00:31:57 +0900

    不要なインデント

--- magicians.py
+++ magicians.py
@@ -3,8 +3,8 @@ for magician in magicians:
 #print (f"{magician.title()}は素晴らしい手品を演じた！")インデントを忘れるとエラーが発生します。
 	print (f"{magician.title()}は素晴らしい手品を演じた！")
 	print (f"{magician.title()}の次の手品が待ちきれない。\n") #\nにより最後に空行
-print(f"{magician.title()}は素晴らしい手品を演じた。")
-
+#print(f"{magician.title()}は素晴らしい手品を演じた。")
+	print("みなさんありがとう。素晴らしい手品ショーでした！")
 print("みなさんありがとう。素晴らしい手品ショーでした！")
 
 
