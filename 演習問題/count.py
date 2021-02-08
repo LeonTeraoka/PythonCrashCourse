b2e024eceff2c69d7d80f83b5c9db1116d1aa6c0
LeonTeraoka <brt765g@gmail.com>
2021-02-08 23:18:57 +0900

    演習問題4-3-6

---
+++ 演習問題/count.py
@@ -0,0 +1,15 @@
+#for twenty in range(1, 21):
+#	print(twenty)
+
+#for million in range(1, 1000001):
+	#print(million)
+
+
+million = [million for million in range(1, 1000001)]
+print(f"{min(million)}")
+print(f"{max(million)}")
+print(f"{sum(million)}")
+
+twenty = [twenty for twenty in range(1, 21, 1)]
+print(twenty)
+
