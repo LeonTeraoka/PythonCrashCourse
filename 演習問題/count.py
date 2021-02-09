33962d828a85b609115916fd1e6776651b021ed6
LeonTeraoka <brt765g@gmail.com>
2021-02-09 23:32:18 +0900

    演習問題4-7-9

--- 演習問題/count.py
+++ 演習問題/count.py
@@ -13,3 +13,14 @@ print(f"{sum(million)}")
 twenty = [twenty for twenty in range(1, 21, 1)]
 print(twenty)
 
+thirty = [value for value in range(3, 33, 3)]
+print(thirty)
+
+cubic = []
+for value in range(1, 11):
+	cubic = value**3
+	print(cubic)
+
+
+cubic_number = [value**3 for value in range (1, 11)]
+print(cubic_number)
