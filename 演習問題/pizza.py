44e964dd9b9e759b6430d6c67a049963a47fed04
LeonTeraoka <brt765g@gmail.com>
2021-02-11 12:30:53 +0900

    演習問題4-11

--- 演習問題/pizza.py
+++ 演習問題/pizza.py
@@ -1,4 +1,9 @@
-pizzas = ["chicago", "sicilian", "greek"]
+pizzas = ["chicago", "sicilian", "greek", "meat"]
+friend_pizzas = pizzas[:]
+friend_pizzas.append("pepperoni")
+print(pizzas)
+print(friend_pizzas)
+
 for pizza in pizzas:
 	print(f"私は{pizza.title()}が好きです。")
 print("私はピザが大好きです！")
