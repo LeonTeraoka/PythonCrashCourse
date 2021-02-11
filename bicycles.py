199351edd0c9b66804dc0d68048ec30b658fd412
LeonTeraoka <brt765g@gmail.com>
2021-02-11 11:32:55 +0900

    演習問題4-10

--- bicycles.py
+++ bicycles.py
@@ -1,5 +1,10 @@
-bicycles = ["trek", "cannondale", "redline", "specialized"]
-print(bicycles)
+bicycles = ["trek", "cannondale", "redline", "specialized", "woom"]
+print("リストの最初の3つの要素です。")
+print(bicycles[:3])
+print("リストの中央の3つの要素です。")
+print(bicycles[1:4])
+print("リストの最後の3つの要素です。")
+print(bicycles[2:])
 print("----------------------------")
 print(bicycles[0])
 print(bicycles[0].title())
