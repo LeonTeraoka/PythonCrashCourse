5313079ab8511b5a20dc64d677f7de8704c813ce
LeonTeraoka <brt765g@gmail.com>
2021-02-13 06:12:15 +0900

    タプルを上書き

--- dimensions.py
+++ dimensions.py
@@ -1,7 +1,13 @@
+print("元のサイズ")
 dimensions = (200, 50)
+for dimension in dimensions:
+	print(dimension)
+
 #dimensions[0] = 250　⇦エラーで返ってくる
 print(dimensions[0])
 print(dimensions[1])
+
+print("変更したサイズ")
+dimensions = (400, 100)
 for dimension in dimensions:
 	print(dimension)
-	
