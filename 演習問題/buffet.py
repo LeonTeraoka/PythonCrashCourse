b9659fab164c2f42152f4c87357a95afe18f5620
LeonTeraoka <brt765g@gmail.com>
2021-02-13 22:14:30 +0900

    演習問題4-13

---
+++ 演習問題/buffet.py
@@ -0,0 +1,10 @@
+buffet = ("ライス", "味噌汁", "豚汁", "カレー", "うどん" )
+for menu in buffet:
+	print(menu)
+	#buffet[0] = ("パン") エラー
+print("--------------------")
+
+buffet = ("クロワッサン", "ハンバーガー", "豚汁", "カレー", "うどん" )
+for menu in buffet:
+	print(menu)
+
