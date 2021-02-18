cd1d28396115dcaaf0c02074cabf76099e9b90e7
LeonTeraoka <brt765g@gmail.com>
2021-02-18 23:21:47 +0900

    演習問題5-1

---
+++ 演習問題/car.py
@@ -0,0 +1,31 @@
+car = "subaru"
+print("car == 'subaru' の結果を True と予測します。")
+print(car == 'subaru')
+
+print("\ncar == 'audi' の結果を False と予測します。")
+print(car == 'audi')
+
+car = "audi" 
+print("car == 'subaru' の結果を False と予測します。")
+print(car == 'subaru')
+
+print("\ncar == 'audi' の結果を True と予測します。")
+print(car == 'audi')
+
+car = "bmw"
+print("\ncar == 'subaru' の結果を False と予測します。")
+print(car == 'subaru')
+
+print("\ncar == 'audi' の結果を False と予測します。")
+print(car == 'audi')
+
+print("\ncar == 'bmw' の結果を True と予測します。")
+print(car == 'bmw')
+
+car = "toyota"
+print("\ncar == 'toyota' の結果を True と予測します。")
+print(car == 'toyota')
+
+car = "日産"
+print("\ncar == '日産' の結果を True と予測します。")
+print(car == '日産')
