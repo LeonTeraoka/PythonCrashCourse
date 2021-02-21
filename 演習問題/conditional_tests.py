38bcaa7b2a379af139bd760021fdc2db49079981
LeonTeraoka <brt765g@gmail.com>
2021-02-21 10:28:54 +0900

    演習問題5-2,and/orを使った文検討中

--- 演習問題/conditional_tests.py
+++ 演習問題/conditional_tests.py
@@ -1,5 +1,35 @@
-hi = "hello"
-if hi == "hello":
+greeting = "Hello"
+if greeting == "Hello":
 	print("true")
-else:
+if greeting == "yo":
 	print("false")
+if greeting.lower() == "hello":
+	print("true")
+if greeting.lower() == "Hello":
+	print("false")
+
+number = 20
+if number = 20:
+	print("true")
+if number = 19:
+	print("false")
+if number >= 19:
+	print("true")
+if number >= 21:
+	print("false")
+if number <= 21:
+	print("true")
+if number <= 19:
+	print("false")
+if number (number_0 >= 21) and (number_1 ):
+	print("true")
+
+
+
+#reetings = ["Hello", "hi", "hey",]
+#print(greetings)
+#for greeting in greetings:
+#	if greeting == "yo":
+#		print("false")
+#	if greeting == "Hello":
+#		print("true")
