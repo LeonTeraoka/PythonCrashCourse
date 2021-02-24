8bc3f9ff2b756ab3c693d6e2de186f94b7259610
LeonTeraoka <brt765g@gmail.com>
2021-02-24 20:12:46 +0900

    if-elif-else文

--- amusement_park.py
+++ amusement_park.py
@@ -1,7 +1,11 @@
 age = 12
 if age < 4:
-	print("入場料金は0円です。")
+#	print("入場料金は0円です。")
+	price = 0
 elif age < 18:
-	print("入場料金は2500円です。")
-else:
-	print("入場料金は4000円です。")
+#	print("入場料金は2500円です。")
+	price = 2500
+#else:
+	price = 4000
+#	print("入場料金は4000円です。")
+print(f"入場料金は{price}円です。")
