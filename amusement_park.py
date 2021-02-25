8d7d9b758d4f1e4024cadd0992ad12e760674794
LeonTeraoka <brt765g@gmail.com>
2021-02-25 23:17:00 +0900

    複数のelifブロックを使用

--- amusement_park.py
+++ amusement_park.py
@@ -5,7 +5,9 @@ if age < 4:
 elif age < 18:
 #	print("入場料金は2500円です。")
 	price = 2500
-#else:
+elif age <65:
 	price = 4000
+#else:
+	price = 2000
 #	print("入場料金は4000円です。")
 print(f"入場料金は{price}円です。")
