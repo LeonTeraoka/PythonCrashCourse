d7af9b705391248708901ca7f578feae15477f24
LeonTeraoka <brt765g@gmail.com>
2021-02-27 23:49:34 +0900

    複数の条件をテスト

--- toppings.py
+++ toppings.py
@@ -1,4 +1,12 @@
-requested_topping = "マッシュルーム"
+requested_topping = ["マッシュルーム", "エクストラチーズ"]
 
-if requested_topping != "アンチョビ":
-    print("アンチョビを注文してください！")
+#if requested_topping != "アンチョビ":
+#    print("アンチョビを注文してください！")
+if "マッシュルーム" in requested_topping:
+	print("マッシュルームを追加する。")
+elif "ペパロニ" in requested_topping:
+	print("ペパロニを追加する。")
+elif "エクストラチーズ" in requested_topping:
+	print("エクストラチーズを追加する。")
+
+print("\nピザができました！")
