9f2d6e71b26c935a27bb2038b4fa9d61515d1f99
LeonTeraoka <brt765g@gmail.com>
2021-03-01 23:49:53 +0900

    演習問題5-5

--- 演習問題/alien.py
+++ 演習問題/alien.py
@@ -1,7 +1,13 @@
 alien_color = "青"
-if alien_color == "赤":
-	print("何も出力されません。")
+#if alien_color == "赤":
+#	print("何も出力されません。")
 if alien_color == "緑":
-	print("プレイヤーが5点獲得")
-else:
-	print("プレイヤーが10点獲得")
+	point = 5
+	print(f"プレイヤーは {point} を獲得した。")
+elif alien_color == "黄":
+	point = 10
+	print(f"プレイヤーは {point} を獲得した。")
+elif alien_color == "赤":
+	point = 15
+	print(f"プレイヤーは {point} を獲得した。")
+#print(f"プレイヤーは {point} を獲得した。")
