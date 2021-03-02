06e2107c6d577febb372a1e36200cd9ca923a789
LeonTeraoka <brt765g@gmail.com>
2021-03-02 21:24:11 +0900

    演習問題5-6

---
+++ 演習問題/life_stage.py
@@ -0,0 +1,16 @@
+age = 66
+
+if age <= 2:
+	print("赤ちゃん")
+if age == 2 < 4:
+	print("幼児")
+if age <= 13:
+	print("子ども")
+if age <= 20:
+	print("ティーンエイジャー")
+if age <= 65:
+	print("大人")
+if age >= 65:
+	print("高齢者")
+
+
