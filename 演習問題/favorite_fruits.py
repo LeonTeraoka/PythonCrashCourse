03860d4cf9ca2d10b94e9474d3e726c8214dee4c
LeonTeraoka <brt765g@gmail.com>
2021-03-03 23:40:32 +0900

    演習問題5-7

---
+++ 演習問題/favorite_fruits.py
@@ -0,0 +1,12 @@
+fruits = ["grape", "banana", "apple", "lemon", "orange"]
+for favorite in fruits:
+	if favorite == "grape":
+		print("あなたは本当にgrapeが好きですね！")
+	if favorite == "banana":
+		print("nothing")
+	if favorite == "apple":
+		print("nothing")
+	if favorite == "lemon":
+		print("nothing")
+	if favorite == "orange":
+		print("nothing")
