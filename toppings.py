e474631da27a51cc7b4b9a3085fdf2c3fb297169
LeonTeraoka <brt765g@gmail.com>
2021-03-04 23:51:31 +0900

    特別な要素を確認1

--- toppings.py
+++ toppings.py
@@ -1,12 +1,15 @@
-requested_topping = ["マッシュルーム", "エクストラチーズ"]
+requested_toppings = ["マッシュルーム","ピーマン", "エクストラチーズ"]
+
+for requested_topping in requested_toppings:
+	print(f"ピザに{requested_topping}を追加します。")
 
 #if requested_topping != "アンチョビ":
 #    print("アンチョビを注文してください！")
-if "マッシュルーム" in requested_topping:
-	print("マッシュルームを追加する。")
-elif "ペパロニ" in requested_topping:
-	print("ペパロニを追加する。")
-elif "エクストラチーズ" in requested_topping:
-	print("エクストラチーズを追加する。")
+#if "マッシュルーム" in requested_topping:
+#	print("マッシュルームを追加する。")
+#elif "ペパロニ" in requested_topping:
+#	print("ペパロニを追加する。")
+#elif "エクストラチーズ" in requested_topping:
+#	print("エクストラチーズを追加する。")
 
 print("\nピザができました！")
