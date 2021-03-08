fd6d1557826712ba9f5d6b10afc0cb3ff82f206e
LeonTeraoka <brt765g@gmail.com>
2021-03-08 23:24:45 +0900

    演習問題5-8

---
+++ 演習問題/user.py
@@ -0,0 +1,9 @@
+user_name = ["admin", "muffin", "muffn", "milk", "mint"]
+
+for user_name in user_name:
+	print(f"こんにちは{user_name.title()}")
+
+if user_name in user_name == "admin":
+	print("こんにちはadmin、状況のレポートを見ますか？")
+else:
+	print("こんにちはJaden、またログインしてくれてありがとう。")
