f674cc256f7c725313250d7d4726f24cb194f3d5
LeonTeraoka <brt765g@gmail.com>
2021-03-13 18:54:07 +0900

    演習問題5-11

---
+++ 演習問題/numbering.py
@@ -0,0 +1,20 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+for number in numbers:
+	if number == 1:
+		print(f"1st")
+	elif number == 2:
+		print(f"2nd")
+	elif number == 3:
+		print(f"3rd")
+	elif number == 4:
+		print(f"4th")
+	elif number == 5:
+		print(f"5th")
+	elif number == 6:
+		print(f"6th")
+	elif number == 7:
+		print(f"7th")
+	elif number == 8:
+		print(f"8th")
+	elif number == 9:
+		print(f"9th")
