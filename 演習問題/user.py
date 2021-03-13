31276947ab03435ff9f996c3e0d900b4790c5276
LeonTeraoka <brt765g@gmail.com>
2021-03-13 18:38:37 +0900

    演習問題5-10

--- 演習問題/user.py
+++ 演習問題/user.py
@@ -1,12 +1,23 @@
-#user_name = ["admin", "muffin", "muffn", "milk", "mint"]
-user_name = []
+current_users = ["admin", "muffin", "muffn", "milk", "mint"]
+new_users = ["noname", "mofin", "muffin", "muffn", "bon"]
 
-if user_name:
-	for user_name in user_name:
-		print(f"こんにちは{user_name.title()}")
+for new_users in new_users:
+	if new_users in current_users:
+		print("使用できません。")
+	else:
+		print("使用できます。")
+
+
+
+
+#user_name = []
+
+#if user_name:
+#	for user_name in user_name:
+#		print(f"こんにちは{user_name.title()}")
 
 #if user_name in user_name == "admin":
 #	print("こんにちはadmin、状況のレポートを見ますか？")
-else:
-	print("ユーザー募集中です！")
+#else:
+#	print("ユーザー募集中です！")
 #	print("こんにちはJaden、またログインしてくれてありがとう。")
