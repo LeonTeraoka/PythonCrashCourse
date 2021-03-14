655cd02e1216bc421d72a222688d6c38e5530e0f
LeonTeraoka <brt765g@gmail.com>
2021-03-14 23:31:29 +0900

    新しいキーと値のペアを追加

--- alien.py
+++ alien.py
@@ -1,6 +1,11 @@
 alien_0 = {"color": "green", "points": 5}
+print(alien_0)
+
+alien_0["x_position"] = 0
+alien_0["y_position"] = 25
+print(alien_0)
 #alien_0 = {"color": "green"}
 
-new_points = alien_0["points"]
-print(alien_0["color"])
-print(f"{new_points}点獲得しました！")
+#new_points = alien_0["points"]
+#print(alien_0["color"])
+#print(f"{new_points}点獲得しました！")
