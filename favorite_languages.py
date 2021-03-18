3d6b557ca5c684c5e13ec98c039e55363d01b2be
LeonTeraoka <brt765g@gmail.com>
2021-03-18 23:20:21 +0900

    似たようなオブジェクトを格納した辞書

---
+++ favorite_languages.py
@@ -0,0 +1,9 @@
+favorite_languages = {
+	"jen": "python",
+	"sarah": "c",
+	"edward": "ruby",
+	"phil": "python",
+}
+
+langueage = favorite_languages["sarah"].title()
+print(f"Sarahの好きなプログラミング言語は{langueage}です。")
