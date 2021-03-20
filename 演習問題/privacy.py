3366c96370f299aed5113049e94a60bf21be9812
LeonTeraoka <brt765g@gmail.com>
2021-03-20 14:47:57 +0900

    演習問題6-1

---
+++ 演習問題/privacy.py
@@ -0,0 +1,6 @@
+muffn = {"first_name": "muffin", "last_name": "fix", "age": 30, "city": "ikebukuro"}
+
+print(muffn["first_name"].title())
+print(muffn["last_name"].title())
+print(muffn["age"])
+print(muffn["city"].title())
