c8667e527b7eaaae63b8a87d20ffc8ad3108a86d
LeonTeraoka <brt765g@gmail.com>
2021-03-22 22:35:53 +0900

    演習問題6-3

---
+++ 演習問題/glossary.py
@@ -0,0 +1,14 @@
+glossary = {"cars": "車の種類", "foods": "好きな食べ物", 
+	"language": "言語の種類", "hello_world": "最初のやつ", "bicycles": "自転車の種類"}
+car = glossary["cars"]
+food = glossary["foods"]
+language = glossary["language"]
+tutorial = glossary["hello_world"]
+bicycle = glossary["bicycles"]
+
+
+print(f"「{car}」のリストを出力するプログラム")
+print(f"\n「{food}」好きな食べ物リストから、好きな食べ物だけを出力するプログラム")
+print(f"\n「{language}」はリストの練習をするために、好きな言語を格納したリスト")
+print(f"\n「{tutorial}」一番最初にやったHello Worldと出力させられるプログラム")
+print(f"\n「{bicycle}」好きな自転車リストから追加したり消したりした練習プログラム")
