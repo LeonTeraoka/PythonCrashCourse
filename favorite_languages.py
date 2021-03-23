d350f87e3011e4675d783e146a95b2947899e060
LeonTeraoka <brt765g@gmail.com>
2021-03-23 22:28:45 +0900

    全てのキーと値のペアをループ

--- favorite_languages.py
+++ favorite_languages.py
@@ -5,5 +5,7 @@ favorite_languages = {
 	"phil": "python",
 }
 
-langueage = favorite_languages["sarah"].title()
-print(f"Sarahの好きなプログラミング言語は{langueage}です。")
+#langueage = favorite_languages["sarah"].title()
+#print(f"Sarahの好きなプログラミング言語は{langueage}です。")
+for name, language in favorite_languages.items():
+	print(f'{name.title()}の好きなプログラミング言語は{language.title()}です。')
