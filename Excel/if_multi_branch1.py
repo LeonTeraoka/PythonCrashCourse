0d299c5a2204be3916e8702a85dfd5792f555703
LeonTeraoka <brt765g@gmail.com>
2021-03-27 23:42:59 +0900

    if〜else〜elifで何通りにも処理を分岐

---
+++ Excel/if_multi_branch1.py
@@ -0,0 +1,13 @@
+score = 75
+a_list = []
+b_list = []
+c_list = []
+if score >= 80:
+	a_list.append(score)
+elif score >= 60:
+	b_list.append(score)
+else:
+	c_list.append(score)
+print(a_list)
+print(b_list)
+print(c_list)
