6da3667eb093c93cbb9e82d4becfc14f3f905099
LeonTeraoka <brt765g@gmail.com>
2021-03-28 03:48:26 +0900

    演習問題6-5

---
+++ 演習問題/river.py
@@ -0,0 +1,7 @@
+rivers = {"mississippi river": "usa",
+	"nile": "egypt", "amazonas": "brazil",
+	}
+for river, site in rivers.items():
+	print(f"\n{river.title()}は{site.title()}で流れている。")
+	print(f"{river.title()}")
+	print(f"{site.title()}")
