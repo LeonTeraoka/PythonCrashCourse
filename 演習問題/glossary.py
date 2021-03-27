9bfa0625e918837170fc7298ab2ed4aa15e692d1
LeonTeraoka <brt765g@gmail.com>
2021-03-27 21:11:36 +0900

    演習問題6-4

--- 演習問題/glossary.py
+++ 演習問題/glossary.py
@@ -1,14 +1,10 @@
-glossary = {"cars": "車の種類", "foods": "好きな食べ物", 
-	"language": "言語の種類", "hello_world": "最初のやつ", "bicycles": "自転車の種類"}
-car = glossary["cars"]
-food = glossary["foods"]
-language = glossary["language"]
-tutorial = glossary["hello_world"]
-bicycle = glossary["bicycles"]
+glossaries = {"print": "出力", "append": "リストに追加", 
+	"title": "頭文字の大文字化", "upper": "大文字化", "lower": "小文字化",
+	"sort": "整頓", "reverse": "逆", "len": "長さを調べる",
+	"del": "要素を削除", "pop": "一時的に要素を削除",
+	}
+for glossary, mean in glossaries.items():
+	print(f"{glossary}、の意味は「{mean}」")
 
 
-print(f"「{car}」のリストを出力するプログラム")
-print(f"\n「{food}」好きな食べ物リストから、好きな食べ物だけを出力するプログラム")
-print(f"\n「{language}」はリストの練習をするために、好きな言語を格納したリスト")
-print(f"\n「{tutorial}」一番最初にやったHello Worldと出力させられるプログラム")
-print(f"\n「{bicycle}」好きな自転車リストから追加したり消したりした練習プログラム")
+
