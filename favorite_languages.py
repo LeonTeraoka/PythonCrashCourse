9e71012a843599f03e8b148b18b35beb3f558e75
LeonTeraoka <brt765g@gmail.com>
2021-03-29 23:18:59 +0900

    演習問題6-6

--- favorite_languages.py
+++ favorite_languages.py
@@ -5,15 +5,22 @@ favorite_languages = {
 	"phil": "python",
 }
 
-print("以下の言語が投票されました。")
-for language in set(favorite_languages.values()):
-	print(language.title())
+not_vote = ["jen", "sarah"]
 
-#for name in sorted(favorite_languages.keys()):
-#	print(f"{name.title()}、投票ありがとう。")
+#print("以下の言語が投票されました。")
+#for language in set(favorite_languages.values()):
+#	print(language.title())
+for name in not_vote:
+	print(f"{name.title()}さん、投票してください！")
 
-#if "erin" not in favorite_languages.keys():
-#	print("Erin、投票してください！")
+del favorite_languages["jen"] 
+del favorite_languages["sarah"]
+
+for name in sorted(favorite_languages.keys()):
+	print(f"{name.title()}、投票ありがとう。")
+
+#if name not in favorite_languages.keys():
+#	print(f"{other.title()}さん投票してください！")
 #langueage = favorite_languages["sarah"].title()
 #print(f"Sarahの好きなプログラミング言語は{langueage}です。")
 #for name, language in favorite_languages.items():
