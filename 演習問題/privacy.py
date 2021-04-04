15495fb52fc4d031eb4946b6c3eb8d8145b64bea
LeonTeraoka <brt765g@gmail.com>
2021-04-04 23:31:52 +0900

    演習問題6-7

--- 演習問題/privacy.py
+++ 演習問題/privacy.py
@@ -1,6 +1,31 @@
-muffn = {"first_name": "muffin", "last_name": "fix", "age": 30, "city": "ikebukuro"}
+people = {
+	"muffn": {
+		"first_name": "muffin", 
+		"last_name": "fix", 
+		"age": 30, 
+		"city": "ikebukuro"
+		},
+	
+	"muffin": {
+		"first_name": "men", 
+		"last_name": "muff", 
+		"age": 25, 
+		"city": "tachikawa"
+		},
+	
+	"mint": {
+		"first_name": "mintia", 
+		"last_name": "men", 
+		"age": 18, 
+		"city": "kitasenju"
+		},
+	}
 
-print(muffn["first_name"].title())
-print(muffn["last_name"].title())
-print(muffn["age"])
-print(muffn["city"].title())
+print(people)
+
+for person, privacy in people.items():
+	print(f"\n名前:{person}")
+	print(f"\n情報:{privacy}")
+#print(muffn["last_name"].title())
+#print(muffn["age"])
+#print(muffn["city"].title())
