9385171a2c82fc888c928dc3134ee71659d84eb0
LeonTeraoka <brt765g@gmail.com>
2021-04-05 22:25:03 +0900

    演習問題6-8

---
+++ 演習問題/pet.py
@@ -0,0 +1,10 @@
+pets = {"leopardgecko": "teraoka",
+	"python": "muffin",
+	"goldfish": "muffin",
+	}
+
+for pet, owner in pets.items():
+	print(f"\n{owner}は{pet}を飼育中")
+	if pet == ["leopardgecko"]:
+		print("\n拒食中")
+
