458fd4395839dee8d51af1c62b594a79348c275d
LeonTeraoka <brt765g@gmail.com>
2021-04-06 21:23:20 +0900

    演習問題6-9

---
+++ 演習問題/favorite_places.py
@@ -0,0 +1,8 @@
+favorite_places = {
+	"muffin": "nerima",
+	"mint": "ikebukuro",
+	"tera": "tachikawa",
+}
+
+for person, place in favorite_places.items():
+	print(f"\n{person}の好きな場所は{place.title()}")
