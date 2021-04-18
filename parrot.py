99ed908a137c5e359386ca8d902272943838920a
LeonTeraoka <brt765g@gmail.com>
2021-04-18 23:45:37 +0900

    停止のタイミングをユーザが選択

--- parrot.py
+++ parrot.py
@@ -1,2 +1,7 @@
-message = input("何か書いてください。繰り返してお返事します：")
-print(message)
+prompt = "\n何か書いてください。繰り返してお返事します。"
+prompt += "\nプログラムを止めるには'終了'と入力してください。"
+message = ""
+while message != "終了":
+	message = input(prompt)
+#message = input("何か書いてください。繰り返してお返事します：")
+	print(message)
