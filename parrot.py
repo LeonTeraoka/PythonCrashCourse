509d5908da1090162fd1b21ba94f88d210865179
LeonTeraoka <brt765g@gmail.com>
2021-04-19 23:40:50 +0900

    フラグを使う

--- parrot.py
+++ parrot.py
@@ -1,7 +1,17 @@
 prompt = "\n何か書いてください。繰り返してお返事します。"
 prompt += "\nプログラムを止めるには'終了'と入力してください。"
-message = ""
-while message != "終了":
+
+active = True
+while active:
 	message = input(prompt)
+
+	if message == "終了":
+		active = False
+	else:
+		print(message)
+
+#message = ""
+#while message != "終了":
+#	message = input(prompt)
 #message = input("何か書いてください。繰り返してお返事します：")
-	print(message)
+#	gitprint(message)
