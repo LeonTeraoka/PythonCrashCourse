bdf34cde454c875297b16f88df2c21eedef5ef4a
LeonTeraoka <brt765g@gmail.com>
2021-05-04 15:35:57 +0900

    演習問題7-9

--- 演習問題/delicatessen.py
+++ 演習問題/delicatessen.py
@@ -1,11 +1,16 @@
-sandwich_orders = ["ハム","ツナ","イチゴ"]
+sandwich_orders = ["ハム", "ツナ", "イチゴ", "パストラミ", "パストラミ", "パストラミ"]
 finished_sandwiches = []
 
 while sandwich_orders:
 	sandwich = sandwich_orders.pop()
-
 	print(f"注文リスト：{sandwich}")
 	finished_sandwiches.append(sandwich)
+
+print(" ")
+
+while "パストラミ" in finished_sandwiches:
+	finished_sandwiches.remove("パストラミ")
+	print("パストラミは品切れです。")
 
 print("\n下記、注文されたサンドウィッチです。")
 for finished_sandwiches in finished_sandwiches:
