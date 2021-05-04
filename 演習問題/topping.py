9c31e8164ae6925606ec104f7836a006595f7e29
LeonTeraoka <brt765g@gmail.com>
2021-05-04 13:15:31 +0900

    演習問題7-6

--- 演習問題/topping.py
+++ 演習問題/topping.py
@@ -2,5 +2,15 @@ topping = "\nトッピングは如何にしますか？"
 topping += "\n選び終えたら「終了」と入力してください"
 topping += "\n追加するトッピング："
 order = ""
-while order != "終了":
+active = True
+while active:
 	order = input(topping)
+	if order == "終了":
+		print("\n以上、ご利用ありがとうございます。")
+		break
+	else:
+		print(f"\n{order}が追加されました。")
+
+
+#while order != "終了":
+#	order = input(topping)
