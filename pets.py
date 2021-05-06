0c71077db2ea802b97576ebb5143ba622e379cdc
LeonTeraoka <brt765g@gmail.com>
2021-05-06 23:18:15 +0900

    デフォルト値

--- pets.py
+++ pets.py
@@ -1,9 +1,9 @@
-def describe_pet(animal_type, pet_name):
+def describe_pet(pet_name, animal_type="イヌ"):
 	"""ペットについての情報を出力する。"""
 	print(f"\n私は{animal_type}を飼っています。")
 	print(f"{animal_type}の名前は{pet_name.title()}です。")
 
-describe_pet(animal_type="フェレット", pet_name="せぶん")
+describe_pet(pet_name="ウィリー")
 
 
 
