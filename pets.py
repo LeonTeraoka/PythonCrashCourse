91f7f3cb01ef4349816c114b5c8696a9debe6676
LeonTeraoka <brt765g@gmail.com>
2021-05-07 23:38:03 +0900

    関数を同じように呼び出す

--- pets.py
+++ pets.py
@@ -4,9 +4,11 @@ def describe_pet(pet_name, animal_type="イヌ"):
 	print(f"{animal_type}の名前は{pet_name.title()}です。")
 
 describe_pet(pet_name="ウィリー")
+describe_pet("ウィリー")
 
-
-
+describe_pet("せぶん", "フェレット")
+describe_pet(pet_name="せぶん", animal_type="フェレット")
+describe_pet(animal_type="フェレット", pet_name="せぶん")
 
 
 #pets = ["dog", "cat", "dog", "goldfish", "cat", "rabbit", "cat"]
