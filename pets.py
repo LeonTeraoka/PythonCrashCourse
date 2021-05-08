619c3d32f90cb23ca58e2184b635a538b1c82588
LeonTeraoka <brt765g@gmail.com>
2021-05-08 23:48:39 +0900

    実引数のエラーを回避

--- pets.py
+++ pets.py
@@ -1,14 +1,15 @@
-def describe_pet(pet_name, animal_type="イヌ"):
+def describe_pet(animal_type, pet_name):
 	"""ペットについての情報を出力する。"""
 	print(f"\n私は{animal_type}を飼っています。")
 	print(f"{animal_type}の名前は{pet_name.title()}です。")
 
-describe_pet(pet_name="ウィリー")
-describe_pet("ウィリー")
+describe_pet()
+#describe_pet(pet_name="ウィリー")
+#describe_pet("ウィリー")
 
-describe_pet("せぶん", "フェレット")
-describe_pet(pet_name="せぶん", animal_type="フェレット")
-describe_pet(animal_type="フェレット", pet_name="せぶん")
+#describe_pet("せぶん", "フェレット")
+#describe_pet(pet_name="せぶん", animal_type="フェレット")
+#describe_pet(animal_type="フェレット", pet_name="せぶん")
 
 
 #pets = ["dog", "cat", "dog", "goldfish", "cat", "rabbit", "cat"]
