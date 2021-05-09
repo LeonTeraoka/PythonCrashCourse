7db3b66e04e6425e6b9a6e7dcae1c6e8b35dd45f
LeonTeraoka <brt765g@gmail.com>
2021-05-09 16:02:58 +0900

    演習問題8-5

---
+++ 演習問題/city.py
@@ -0,0 +1,9 @@
+def describe_city(city, country="アイスランド"):
+	"""国と地域を出力する"""
+	print(f"{city}は{country}にあります。")
+
+describe_city(city="レイキャビグ", country="アイスランド")
+
+describe_city(city="東京", country="日本")
+
+describe_city(city="ローマ", country="イタリア")
