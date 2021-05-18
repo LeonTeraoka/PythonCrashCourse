0831b1fbac618edb312b0263667e09c9d21bf9d7
LeonTeraoka <brt765g@gmail.com>
2021-05-18 23:46:43 +0900

    任意の数の引数を渡す

--- pizza.py
+++ pizza.py
@@ -1,11 +1,25 @@
-pizza = {
-	"crust": "レギュラー",
-	"toppings": ["マッシュルーム", "エクストラチーズ"],
-	}
+def make_pizza(*toppings):
+	"""注文されたピザの要約を出力する"""
+	print("\n以下のトッピングのピザを作ります。")
+	for topping in toppings:
+		print(f"-{topping}")
+
+make_pizza("ペパロニ")
+make_pizza("マッシュルーム","ピーマン","エクストラチーズ")
+
+
+
+
+
+
+#pizza = {
+#	"crust": "レギュラー",
+#	"toppings": ["マッシュルーム", "エクストラチーズ"],
+#	}
 
 #注文の要約
-print(f"あなたが注文したのは{pizza['crust']}生地のピザで、"
-	"トッピングは以下の注文の通りです。")
+#print(f"あなたが注文したのは{pizza['crust']}生地のピザで、"
+#	"トッピングは以下の注文の通りです。")
 
-for topping in pizza["toppings"]:
-	print(f"\t{topping}")
+#for topping in pizza["toppings"]:
+#	print(f"\t{topping}")
