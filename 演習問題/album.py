557c189d5f2489defaf62ee031e1d3860e969a75
LeonTeraoka <brt765g@gmail.com>
2021-05-20 23:41:10 +0900

    演習問題8-8

--- 演習問題/album.py
+++ 演習問題/album.py
@@ -1,9 +1,17 @@
-def make_album(artist, title, songs=None):
+def make_album(artist, title,):
 	"""アルバム情報を辞書で返す"""
-	album = {'artistname': artist, 'titlename': title}
-	if songs:
-		album["songs"] = songs
-	return album
+	album = {}
 
-musician = make_album('kawakami youhei', 'run away', songs=4)
+	making_album = True
+
+	while making_album:
+		artist = input("アーティスト:")
+		title = input("タイトル:")
+		album[artist] = title
+
+		repeat = input("(yes/no)")
+		if repeat == 'no':
+			making_album = False
+
+musician = make_album('kawakami youhei', 'run away',)
 print(musician)
