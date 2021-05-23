e3b8e9d737dde57a4ed177e3163e989154c5d98b
LeonTeraoka <brt765g@gmail.com>
2021-05-23 22:14:37 +0900

    演習問題8-9

--- 演習問題/message.py
+++ 演習問題/message.py
@@ -1,5 +1,23 @@
-def display_message(message):
+def simple_message(message, completed_messages):
 	"""メッセージを出力"""
-	print(f"メッセージ：{message.title()}")
+	while message:
+		msg = message.pop()
+		completed_messages.append(msg)
 
-display_message("hello world")
+def show_messages(completed_messages):
+	"""メッセージを移す"""
+	for completed_message in completed_messages:
+		print(completed_message)
+
+message = ["おはよう", "こんにちは", "こんばんは"]
+completed_messages = []
+
+simple_message(message, completed_messages)
+show_messages(completed_messages)
+
+
+#def display_message(message):
+#	"""メッセージを出力"""
+#	print(f"メッセージ：{message.title()}")
+
+#display_message("hello world")
