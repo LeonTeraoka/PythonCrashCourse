0c957e286e6a0b9e12a6965d55bfd5a8b494959b
LeonTeraoka <brt765g@gmail.com>
2021-05-23 23:07:26 +0900

    演習問題8-11

--- 演習問題/send_message.py
+++ 演習問題/send_message.py
@@ -1,14 +1,18 @@
 def send_message(message, completed_messages):
 	"""メッセージを出力"""
+	print(message)
 	while message:
 		msg = message.pop()
 		print(f"\n送信中：{msg}")
 		completed_messages.append(msg)
 
+
 def sent_messages(completed_messages):
+	print(message)
 	"""メッセージを移す"""
 	for completed_message in completed_messages:
 		print(f"\n送信済:{completed_message}")
+
 
 message = ["おはよう", "こんにちは", "こんばんは"]
 completed_messages = []
