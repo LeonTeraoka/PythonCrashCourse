f1b52cdd5348143e74a620c38f290666145dec47
LeonTeraoka <brt765g@gmail.com>
2021-05-28 12:41:09 +0900

    演習問題8-12

---
+++ 演習問題/sandwich.py
@@ -0,0 +1,7 @@
+def make_sandwich(*ingredients):
+	"""サンドイッチの中身の一覧を出力する"""
+	print(f"サンドイッチの中身は{ingredients}")
+
+make_sandwich("ハム")
+make_sandwich("レタス", "チーズ", "サラミ")
+make_sandwich("チキン", "生クリーム", "いちご")
