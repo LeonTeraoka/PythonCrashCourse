0c5b7a7ff5a0f9b8e6c6dee1f74cb737c1df1edf
LeonTeraoka <brt765g@gmail.com>
2021-06-11 23:12:43 +0900

    イヌのクラスを作成

---
+++ dog.py
@@ -0,0 +1,15 @@
+class Dog:
+	"""イヌをモデル化したシンプルな実装例"""
+
+	def __init__(self, name, age):
+		"""名前と年齢の属性を初期化する"""
+		self.name = name
+		self.age = age
+
+	def sit(self):
+		"""イヌに「おすわり」の命令を実行する"""
+		print(f"{self.name}はおすわりしている。")
+
+	def roll_over(self):
+		"""イヌに「ごろーん」の命令を実行する"""
+		print(f"{self.name}がごろーんした！")
