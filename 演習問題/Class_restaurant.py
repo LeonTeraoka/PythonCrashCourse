7d970bdf4147060be5ec5ff0276c2a6513f1b099
LeonTeraoka <brt765g@gmail.com>
2021-06-13 23:24:06 +0900

    演習問題9-2

--- 演習問題/Class_restaurant.py
+++ 演習問題/Class_restaurant.py
@@ -14,7 +14,15 @@ class Restaurant:
 		"""レストランの開店名と料理の命令を実行"""
 		print(f"{self.menu}料理の種類")
 
-restaurant = Restaurant("松屋", "牛丼")
+first_restaurant = Restaurant("松屋", "牛丼")
+second_restaurant = Restaurant("CoCo壱", "カレー")
+third_restaurant = Restaurant("サイゼ", "ミラノ風ドリア")
 
-print(f"レストラン名：{restaurant.name}")
-print(f"料理の種類：{restaurant.menu}")
+print(f"最初のレストラン名：{first_restaurant.name}")
+print(f"最初の料理の種類：{first_restaurant.menu}")
+
+print(f"\n2つ目のレストラン名：{second_restaurant.name}")
+print(f"2つ目の料理の種類：{second_restaurant.menu}")
+
+print(f"\n3つ目のレストラン名：{third_restaurant.name}")
+print(f"3つ目の料理の種類：{third_restaurant.menu}")
