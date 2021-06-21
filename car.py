806d1fc602aede44901a2a641b16a5fd73c15627
LeonTeraoka <brt765g@gmail.com>
2021-06-21 21:28:57 +0900

    属性の値を変更

--- car.py
+++ car.py
@@ -17,6 +17,31 @@ class Car:
 		"""自動車の走行距離を出力する"""
 		print(f"走行距離は{self.odometer_reading}kmです。")
 
+	def update_odometer(self, km):
+		"""
+		指定された値に走行距離を更新します。
+		走行距離を減らそうとする処理は拒否します。
+		"""
+		if km >= self.odometer_reading:
+			self.odometer_reading = km
+		else:
+			print("走行距離は減らせません！")
+
+	def increment_odometer(self, km):
+		"""指定された距離を走行距離に追加する"""
+		self.odometer_reading += km
+
+my_used_car = Car("subaru", "outback", 2015)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
+
 my_new_car = Car("audi", "a4", 2019)
 print(my_new_car.get_descriptive_name())
+
+my_new_car.update_odometer(23)
 my_new_car.read_odometer()
