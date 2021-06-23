5def60d2061d627ce8b55ac3fd837b932712ea39
LeonTeraoka <brt765g@gmail.com>
2021-06-23 22:12:40 +0900

    演習問題9-5

--- 演習問題/Class_user.py
+++ 演習問題/Class_user.py
@@ -5,6 +5,7 @@ class User:
 		"""ユーザーの名前と名字の属性を格納"""
 		self.first_name = first_name
 		self.last_name = last_name
+		self.login_attempts = 0
 
 	def describe_user(self):
 		"""ユーザーの名字と名前の詳細"""
@@ -12,7 +13,17 @@ class User:
 
 	def greet_user(self):
 		"""ユーザーへのあいさつメッセージ"""
-		print(f"{self.first_name} {self.last_name}さんこんにちは")
+		print(f"{self.first_name} {self.last_name}こんにちは")
+
+	def reset_login_attempts(self, login):
+		if login >= self.login_attempts:
+			self.login_attempts = login
+		else:
+			print("ログインできません。")
+
+	def increment_login_attempts(self, login):
+		self.login_attempts += login
+		print(f"{self.login_attempts}回ログインしました。")
 
 greet1 = User("muffn", "men")
 greet2 = User("mint", "men")
@@ -22,3 +33,9 @@ greet1.greet_user()
 
 greet2.describe_user()
 greet2.greet_user()
+
+greet1.reset_login_attempts(4)
+greet1.increment_login_attempts(3)
+
+greet1.reset_login_attempts(2)
+greet1.increment_login_attempts(1)
