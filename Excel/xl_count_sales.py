317f6e56ba554f132c126dd670907aa57d2dfce5
LeonTeraoka <brt765g@gmail.com>
2021-07-16 23:36:18 +0900

    マスタの数値を集計

--- Excel/xl_count_sales.py
+++ Excel/xl_count_sales.py
@@ -5,7 +5,7 @@ wb_master = openpyxl.load_workbook("顧客マスタ.xlsx")
 ws_master = wb_master["Sheet1"]
 
 # 売上データのブック、シート
-wb_data = openpyxl.load_workbook("売上データ_202007.xlsx")
+wb_data = openpyxl.load_workbook("売上データ_202007.xlsx", data_only=True)
 ws_data = wb_data["Sheet1"]
 
 #顧客マスタの全データリスト
@@ -26,40 +26,47 @@ result_list = []
 for customer in customer_list:
 	customer_id = customer[0]
 	customer_name = customer[1]
+	# 売上の集計
+	sum_sales = 0
 
 	# 売上件数のカウント
-	count_sales = 0
+	#count_sales = 0
 	#売上データの検索
 	for row in ws_data.iter_rows(min_row=4):
 		# 検索条件(売上データの顧客IDが一致)
 		if row[1].value == customer_id:
-			count_sales = count_sales + 1
+			#count_sales = count_sales + 1
+			# 売上の額は[G列(インデックス =6)]
+			sum_sales = sum_sales = row[6].value
 
-	if count_sales > 0:
+	#if count_sales > 0:
+	if sum_sales >0:
 		# 顧客ID、顧客名称、売上件数の３項目を追加
-		result_list.append([customer_id, customer_name, count_sales])
+		result_list.append([customer_id, customer_name, sum_sales])
 
 # 集計結果用シートを追加
-ws_new = wb_data.create_sheet(title="顧客別売上件数")
+#ws_new = wb_data.create_sheet(title="顧客別売上件数")
+ws_new = wb_data.create_sheet(title="顧客別売上")
 # ヘッダー書き込み
-ws_new.append(["顧客ID", "顧客名称", "売上件数"])
+#ws_new.append(["顧客ID", "顧客名称", "売上件数"])
+ws_new.append(["顧客ID", "顧客名称", "売上計"])
 # 集計結果書き込み
 for result in result_list:
 	ws_new.append(result)
 
-id_list = []
+#id_list = []
 # 集計結果書き込み
-for result in result_list:
-	ws_new.append(result)
+#for result in result_list:
+#	ws_new.append(result)
 	# 集計した顧客IDのリストを作成
-	id_list.append(result[0])
+#	id_list.append(result[0])
 
 # 集計されていない顧客を検索
-for row in ws_data.iter_rows(min_row=4):
+#for row in ws_data.iter_rows(min_row=4):
 	# 検索条件(集計した顧客IDのリストに含まれない)
-	if row[1].value not in id_list:
+#	if row[1].value not in id_list:
 		# 集計されていない顧客IDと顧客名称を表示
-		print(row[1].value, row[2].value)
+#		print(row[1].value, row[2].value)
 
 # 別名で保存
-wb_data.save("売上データ_202007_顧客別売上件数.xlsx")
+wb_data.save("売上データ_202007_顧客別売上合計.xlsx")
